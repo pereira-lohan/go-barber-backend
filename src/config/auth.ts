923605d00db07c98422310b5e3828e6c1985cc4a
pereira-lohan <pereira.lohan.dev@gmail.com>
2020-07-20 23:28:45 -0300

    finished create users and appointments and add tests

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: '5b9db47159f62fa4b35c962a35805998',
+    expiresIn: '1d',
+  },
+};
---
+++ src/modules/users/infra/http/controllers/SessionsController.ts
@@ -0,0 +1,24 @@
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+
+import AuthenticateUserService from '@modules/users/services/AuthenticateUserService';
+
+// * Métodos do RESTFul index, show, create, delete, update
+
+export default class SessionsController {
+  public async create(request: Request, response: Response): Promise<Response>{
+    const { email, password } = request.body;
+
+    const authenticateUser = container.resolve(AuthenticateUserService);
+
+    const { user, token } = await authenticateUser.execute({
+      email,
+      password,
+    });
+
+    delete user.password;
+
+    return response.json({ user, token });
+  }
+
+}
